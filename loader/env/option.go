b2b07260e1e9082220ceabfe499fb97c3d230a20
nikitashtimenko <nikitashtimenko.dev@gmail.com>
2025-05-23 23:02:55 +0200

    feature(loader/env): add WithEnvOptions for passing options to underlying env parser

--- loader/env/option.go
+++ loader/env/option.go
@@ -1,8 +1,11 @@
 package env
 
+import "github.com/caarlos0/env/v11"
+
 // Options defines a set of functional options for the environment loader
 type Options struct {
 	SkipMissingFiles bool
+	EnvOptions       env.Options
 }
 
 // Option defines a functional option for the environment loader
@@ -12,6 +15,14 @@ type Option func(*Options) error
 func WithSkipMissingFiles() Option {
 	return func(opts *Options) error {
 		opts.SkipMissingFiles = true
+		return nil
+	}
+}
+
+// WithEnvOptions allows passing through options to the underlying env parser
+func WithEnvOptions(envOptions env.Options) Option {
+	return func(opts *Options) error {
+		opts.EnvOptions = envOptions
 		return nil
 	}
 }
